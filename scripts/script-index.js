1d6ebc9e58d25b1810992b318ade8c8ad96c569a
Luka <luka68231@gmail.com>
2025-05-25 14:47:07 -0400

    update iwndex.html | update script-index.js | update index.css

--- scripts/script-index.js
+++ scripts/script-index.js
@@ -1,23 +1,24 @@
-document.getElementById('silverPlan').addEventListener('click', function() {
+document.getElementById('basic').addEventListener('click', function() {
     const email = 'luka41935@gmail.com';
-    const subject = 'None';
-    const body = "Hello, I’m interested in purchasing the Silver Plan from your website. Could you kindly assist me with the process?";
+    const subject = 'Purchase';
+    const body = "Hello, I’m interested in...\n\nCould you kindly assist me with the process?";
 
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-})
+});
 
-document.getElementById('goldPlan').addEventListener('click', function() {
+document.getElementById('full').addEventListener('click', function() {
     const email = 'luka41935@gmail.com';
-    const subject = 'None';
-    const body = "Hello, I’m interested in purchasing the Gold Plan from your website. Could you kindly assist me with the process?";
+    const subject = 'Purchase';
+    const body = "Hello, I’m interested in...\n\nCould you kindly assist me with the process?";
 
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-})
+});
 
-document.getElementById('diamondPlan').addEventListener('click', function() {
+document.getElementById('enterprise').addEventListener('click', function() {
     const email = 'luka41935@gmail.com';
-    const subject = 'None';
-    const body = "Hello, I’m interested in purchasing the Diamond Plan from your website. Could you kindly assist me with the process?";
+    const subject = 'Purchase';
+    const body = "Hello, I’m interested in...\n\nCould you kindly assist me with the process?";
 
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
-})
+});
+

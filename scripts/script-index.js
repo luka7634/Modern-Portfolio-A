6384c94ded84cb5497f1eb91f92ff1315be9b50b
Luka <luka68231@gmail.com>
2025-05-25 11:21:26 -0400

    Update script-index.js

--- scripts/script-index.js
+++ scripts/script-index.js
@@ -5,3 +5,19 @@ document.getElementById('silverPlan').addEventListener('click', function() {
 
     window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
 })
+
+document.getElementById('goldPlan').addEventListener('click', function() {
+    const email = 'luka41935@gmail.com';
+    const subject = 'None';
+    const body = "Hello, I’m interested in purchasing the Gold Plan from your website. Could you kindly assist me with the process?";
+
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+})
+
+document.getElementById('diamondPlan').addEventListener('click', function() {
+    const email = 'luka41935@gmail.com';
+    const subject = 'None';
+    const body = "Hello, I’m interested in purchasing the Diamond Plan from your website. Could you kindly assist me with the process?";
+
+    window.location.href = `mailto:${email}?subject=${encodeURIComponent(subject)}&body=${encodeURIComponent(body)}`;
+})
